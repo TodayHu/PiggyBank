c993fdb5ee189ee9f518a07205997c8e421e703a
Daniel Lam <daniel.cklam@gmail.com>
2015-04-13 21:32:30 +1000

    Initial Commit

---
+++ PiggyBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PiggyBank
+//
+//  Created by Daniel Lam on 13/04/2015.
+//  Copyright (c) 2015 Hungry Source Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
