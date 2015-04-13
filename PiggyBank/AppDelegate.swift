15565ae23cbdf8e897076eda55e6ed2945d35ddf
Daniel Lam <daniel.cklam@gmail.com>
2015-04-13 22:14:50 +1000

    Load view controller programmatically. Used white background.

--- PiggyBank/AppDelegate.swift
+++ PiggyBank/AppDelegate.swift
@@ -15,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.whiteColor()
+            window.makeKeyAndVisible()
+            
+            var mainVC  = ViewController()
+            window.rootViewController = mainVC
+        }
+
         // Override point for customization after application launch.
         return true
     }
