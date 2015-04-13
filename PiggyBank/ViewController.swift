53f9f269ecd4c6fba618915cf4f93e691b0a94d9
Daniel Lam <daniel.cklam@gmail.com>
2015-04-14 08:15:46 +1000

    Added piggybank image.

--- PiggyBank/ViewController.swift
+++ PiggyBank/ViewController.swift
@@ -10,7 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var piggybankIV: UIImageView?
+    private let kImageWidth = CGFloat(300)
+    private let kImageHeight = CGFloat(300)
+
+    private var piggybankIV: UIImageView!
+
+    // MARK: - Init
+    
+    convenience override init() {
+        self.init(nibName: nil, bundle: nil)
+        
+        piggybankIV = UIImageView(forAutoLayout: ())
+        piggybankIV.image = UIImage(named: "piggybank-640")
+
+    }
+    
+    /// This initializer is always called by the system because it's the designated initializer.
+    /// This must be defined to to prevent run time error:
+    ///   fatal error: use of unimplemented initializer 'init(nibName:bundle:)' for class 'Flow.FlowViewController'
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    /// This initializer is only called if Storyboard is used.
+    /// This needs to be defined to to prevent compile error:
+    ///   'required' initializer 'init(coder:)' must be provided by subclass of 'UIViewController'
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    } 
+
+    // MARK: - View Life Cycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +48,11 @@ class ViewController: UIViewController {
 
         view.backgroundColor = UIColor.whiteColor()
 
-        piggybankIV = UIImageView(forAutoLayout: ())
+        view.addSubview(piggybankIV)
+        piggybankIV.autoAlignAxisToSuperviewAxis(.Vertical)
+        piggybankIV.autoPinEdgeToSuperviewEdge(.Top, withInset: 20)
+        piggybankIV.autoSetDimension(.Width, toSize: kImageWidth)
+        piggybankIV.autoSetDimension(.Height, toSize: kImageHeight)
     }
 
     override func didReceiveMemoryWarning() {
