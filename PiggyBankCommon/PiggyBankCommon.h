f08db6424548aca352e0f79447b59d8a97ceba12
Daniel Lam <daniel.cklam@gmail.com>
2015-04-19 00:14:39 +1000

    Added new target PiggyBankCommon, a Cocoa Touch Framework.

---
+++ PiggyBankCommon/PiggyBankCommon.h
@@ -0,0 +1,19 @@
+//
+//  PiggyBankCommon.h
+//  PiggyBankCommon
+//
+//  Created by Daniel Lam on 19/04/2015.
+//  Copyright (c) 2015 Hungry Source Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PiggyBankCommon.
+FOUNDATION_EXPORT double PiggyBankCommonVersionNumber;
+
+//! Project version string for PiggyBankCommon.
+FOUNDATION_EXPORT const unsigned char PiggyBankCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PiggyBankCommon/PublicHeader.h>
+
+
