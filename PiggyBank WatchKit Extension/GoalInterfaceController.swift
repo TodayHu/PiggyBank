d45fc9b84a3d66863ce9cb2e9e1d0de111b90f5a
Daniel Lam <daniel.cklam@gmail.com>
2015-04-20 04:27:48 +1000

    Watch - Added basic InterfaceController for the Goal screen.

---
+++ PiggyBank WatchKit Extension/GoalInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  GoalInterfaceController.swift
+//  PiggyBank
+//
+//  Created by Daniel Lam on 20/04/2015.
+//  Copyright (c) 2015 Hungry Source Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+import WatchKit
+import Foundation
+import PiggyBankCommon
+
+class GoalInterfaceController: WKInterfaceController {
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+}
