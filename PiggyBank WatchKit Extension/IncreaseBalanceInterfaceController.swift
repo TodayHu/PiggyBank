8800eab8556d63fa02a07eddca53a485fcb76cff
Daniel Lam <daniel.cklam@gmail.com>
2015-04-19 15:11:50 +1000

    Add new file - IncreaseBalanceInterfaceController.

---
+++ PiggyBank WatchKit Extension/IncreaseBalanceInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  IncreaseBalanceInterfaceController.swift
+//  PiggyBank
+//
+//  Created by Daniel Lam on 19/04/2015.
+//  Copyright (c) 2015 Hungry Source Pty Ltd. All rights reserved.
+//
+
+import Foundation
