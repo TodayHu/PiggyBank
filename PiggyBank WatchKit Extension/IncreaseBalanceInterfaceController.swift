29e53471e5189b5a3696be6204d8b710fb2a7320
Daniel Lam <daniel.cklam@gmail.com>
2015-04-19 15:15:50 +1000

    Added basic methods to IncreaseBalanceInterfaceController and linked from storyboard.

--- PiggyBank WatchKit Extension/IncreaseBalanceInterfaceController.swift
+++ PiggyBank WatchKit Extension/IncreaseBalanceInterfaceController.swift
@@ -6,4 +6,30 @@
 //  Copyright (c) 2015 Hungry Source Pty Ltd. All rights reserved.
 //
 
+import WatchKit
 import Foundation
+import PiggyBankCommon
+
+class IncreaseBalanceInterfaceController: WKInterfaceController {
+
+    private var balanceModel: BalanceModel!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+
+        // Configure interface objects here.
+        balanceModel = BalanceModel()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
+
